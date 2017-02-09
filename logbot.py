3405742c48efe7ab421781a05fe156c04024af75
David <DavidVallee@null.net>
2017-02-09 22:44:52 +0100

    Create logbot.py

---
+++ logbot.py
@@ -0,0 +1,18 @@
+import string
+import socket
+import sys
+import os
+import random
+server = "127.0.0.1"      
+channel = "#BOT"
+botnick = "DAVID" 
+irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+print "connecting to:"+server
+irc.connect((server, 6667))                                             
+irc.send("USER "+ botnick +" "+ botnick +" "+ botnick +" :DAVID\n") 
+irc.send("NICK "+ botnick +"\n")                
+irc.send("PRIVMSG nickserv :DAVID\r\n")    
+irc.send("JOIN "+ channel +"\n")       
+while 1:   
+   text=irc.recv(2040) 
+   print text
